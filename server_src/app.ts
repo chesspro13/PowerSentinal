23e544412e098503b8b68cbe43a0b6d631c6d1c0
Chesspro13 <chesspro13@gmail.com>
2024-07-21 14:19:54 -0700

    Split database variables

--- server_src/app.ts
+++ server_src/app.ts
@@ -2,144 +2,17 @@ import { Request, Response } from "express";
 import * as child from "child_process";
 import Database from "better-sqlite3";
 import express from "express";
-// import {
-//     databaseTable,
-//     prepareDataInsert,
-//     preparedJson,
-// } from "./database_table";
+import {
+    databaseTable,
+    prepareDataInsert,
+    preparedJson,
+} from "./database_table.js";
+import { router as dataRouter } from "./routes/data.js";
 import { config } from "dotenv";
-
-export const databaseTable = `
-    CREATE TABLE IF NOT EXISTS power_data (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    ACTIVE BOOLEAN,
-    APC STRING,
-    DATE STRING,
-    HOSTNAME STRING,
-    VERSION STRING,
-    UPSNAME STRING,
-    CABLE STRING,
-    DRIVER STRING,
-    UPSMODE STRING,
-    STARTTIME STRING,
-    MODEL STRING,
-    STATUS STRING,
-    LINEV FLOAT,
-    LOADPCT FLOAT,
-    BCHARGE FLOAT,
-    TIMELEFT FLOAT,
-    MBATTCHG INTEGER,
-    MINTIMEL INTEGER,
-    MAXTIME INTEGER,
-    SENSE STRING,
-    LOTRANS FLOAT,
-    HITRANS FLOAT,
-    ALARMDEL STRING,
-    BATTV FLOAT,
-    LASTXFER STRING,
-    NUMXFERS INTEGER,
-    TONBATT INTEGER,
-    CUMONBATT INTEGER,
-    XOFFBATT STRING,
-    SELFTEST STRING,
-    STATFLAG STRING,
-    SERIALNO STRING,
-    BATTDATE STRING,
-    NOMINV FLOAT,
-    NOMBATTV FLOAT,
-    NOMPOWER INTEGER,
-    FIRMWARE STRING,
-    END_APC STRING
-    )
-`;
-
-export const preparedJson = `{
-    "ACTIVE": null,
-    "APC": null,
-    "DATE": null,
-    "HOSTNAME": null,
-    "VERSION": null,
-    "UPSNAME": null,
-    "CABLE": null,
-    "DRIVER": null,
-    "UPSMODE": null,
-    "STARTTIME": null,
-    "MODEL": null,
-    "STATUS": null,
-    "LINEV": null,
-    "LOADPCT": null,
-    "BCHARGE": null,
-    "TIMELEFT": null,
-    "MBATTCHG": null,
-    "MINTIMEL": null,
-    "MAXTIME": null,
-    "SENSE": null,
-    "LOTRANS": null,
-    "HITRANS": null,
-    "ALARMDEL": null,
-    "BATTV": null,
-    "LASTXFER": null,
-    "NUMXFERS": null,
-    "TONBATT": null,
-    "CUMONBATT": null,
-    "XOFFBATT": null,
-    "SELFTEST": null,
-    "STATFLAG": null,
-    "SERIALNO": null,
-    "BATTDATE": null,
-    "NOMINV": null,
-    "NOMBATTV": null,
-    "NOMPOWER": null,
-    "FIRMWARE": null,
-    "END_APC": null
-}`;
-
-export const prepareDataInsert = `
-    INSERT INTO power_data VALUES(
-    NULL,
-    :ACTIVE,
-    :APC,
-    :DATE,
-    :HOSTNAME,
-    :VERSION,
-    :UPSNAME,
-    :CABLE,
-    :DRIVER,
-    :UPSMODE,
-    :STARTTIME,
-    :MODEL,
-    :STATUS,
-    :LINEV,
-    :LOADPCT,
-    :BCHARGE,
-    :TIMELEFT,
-    :MBATTCHG,
-    :MINTIMEL,
-    :MAXTIME,
-    :SENSE,
-    :LOTRANS,
-    :HITRANS,
-    :ALARMDEL,
-    :BATTV,
-    :LASTXFER,
-    :NUMXFERS,
-    :TONBATT,
-    :CUMONBATT,
-    :XOFFBATT,
-    :SELFTEST,
-    :STATFLAG,
-    :SERIALNO,
-    :BATTDATE,
-    :NOMINV,
-    :NOMBATTV,
-    :NOMPOWER,
-    :FIRMWARE,
-    :END_APC
-    )
-`;
 
 const app = express();
 app.use(express.json());
+app.use("/data", dataRouter);
 
 config();
 const PORT = process.env.PORT || 27415;
@@ -226,6 +99,5 @@ setInterval(() => {
         newData.run(JSON.parse(prep));
         // db.close();
     });
-    if ( process.env.MODE == "TESTING")
-        getLast()
+    if (process.env.MODE == "TESTING") getLast();
 }, (interval !== undefined ? parseInt(interval) : defaultInterval) * 1000);
