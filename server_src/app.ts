e8bb902f8bdb6cfb1d0f786edd95cad6ecd745a8
Chesspro13 <chesspro13@gmail.com>
2024-07-21 15:20:32 -0700

    Data collection moved to its own file

--- server_src/app.ts
+++ server_src/app.ts
@@ -1,66 +1,28 @@
-import { Request, Response } from "express";
-import * as child from "child_process";
-import Database from "better-sqlite3";
 import express from "express";
-import {
-    databaseTable,
-    prepareDataInsert,
-    preparedJson,
-} from "./database_table.js";
+import { preparedJson } from "./database_table.js";
 import * as databaseOperations from "./database.js";
 import { router as dataRouter } from "./routes/data.js";
 import { router as statusRouter } from "./routes/status.js";
 import { config } from "dotenv";
+import { get_data } from "./data_collector.js";
 
-const app = express();
-app.use(express.json());
-app.use("/data", dataRouter);
-app.use("/status", statusRouter);
-
+// Environment Variables
 config();
 const PORT = process.env.PORT || 27415;
 const defaultInterval = 60;
 const interval =
     process.env.POWER_LOGGING_INTERVAL || defaultInterval.toString();
 
+// Express
+const app = express();
+app.use(express.json());
+app.use("/data", dataRouter);
+app.use("/status", statusRouter);
 app.listen(PORT, () => {
     console.log("Server listening on port: ", PORT);
 });
 
-function get_data(fctn: CallableFunction) {
-    child.exec(
-        "apcaccess",
-        (
-            error: child.ExecException | null,
-            stdout: string | Buffer,
-            stderr: string | Buffer
-        ) => {
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-
-            var jsonOutput = '"ACTIVE":"TRUE"';
-            let q = { Status: "Running" };
-            stdout
-                .toString()
-                .split("\n")
-                .forEach((item) => {
-                    if (item !== "") {
-                        const formattedItem = item.replace(/\s+/g, " ").trim();
-                        let keyVal = formattedItem.split(":");
-                        jsonOutput +=
-                            ',"' +
-                            keyVal[0].trim().replace(" ", "_") +
-                            '":"' +
-                            keyVal[1].trim() +
-                            '"';
-                    }
-                });
-            fctn("{" + jsonOutput + "}");
-        }
-    );
-}
-
+// Data collection loop
 setInterval(() => {
     get_data((a: string) => {
         let prep = preparedJson;
