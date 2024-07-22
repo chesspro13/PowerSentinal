9fd7f5351c902215304f9eb78cff1bebea01ddda
Chesspro13 <chesspro13@gmail.com>
2024-07-21 22:18:44 -0700

    Live view working

--- src/Content.tsx
+++ src/Content.tsx
@@ -2,18 +2,37 @@ import * as React from "react";
 import "./App.css";
 
 function Header() {
-    const [allData, setAllData] = React.useState([]);
+    const [data, setData] = React.useState([]);
+
+    function updateData() {
+        fetch("/api/status").then((result) => {
+            result.json().then((data) => {
+                setData(data);
+                console.log(data);
+            });
+        });
+    }
 
     React.useEffect(() => {
-        fetch("/api/status").then((result) => {
-            console.log(result);
-        });
-    });
+        updateData();
+        const interval = setInterval(() => {
+            updateData();
+        }, 60 * 1000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <main>
             <h1>Main Content</h1>
-            <div id="graph"></div>
+            <div id="graph">
+                {Object.keys(data).map((key) => (
+                    <div className="json-item">
+                        <h2>{key}</h2>
+                        <p>{data[key]}</p>
+                    </div>
+                ))}
+            </div>
         </main>
     );
 }
