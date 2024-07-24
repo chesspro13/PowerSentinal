6a12938b10f83fd583a73d448076978e10c46df8
Chesspro13 <chesspro13@gmail.com>
2024-07-24 11:49:12 -0700

    Updated the installer

--- setup.sh
+++ setup.sh
@@ -13,17 +13,18 @@ dir=$PWD
 subD=${dir//"/home/"/}
 user=${subD%%\/*}
 
+express_install=false
+server_install=false
+client_install=false
+
 serverPackages()
 {
-    ./uninstall 
+    ./uninstall.sh
 
     echo "The following packages are required for opperation: APCUPSD, NVM"
 
-    read -p "Begin the installation of dependencies? [Y/n]: " answer
-    
-    answer="${answer:-Y}"
-    
-    if [[ $answer =~ [Yy] ]]; then
+    choice=$(getUserinput "Begin the installation of dependencies?")
+    if [[ $choice =~ [Yy] ]]; then
         echo "Installing APCUPSD, NodeJS, and UFW"
         apt-get -y install apcupsd nodejs ufw
         echo "APCUPSD, NodeJS, and UFW installed."
@@ -32,32 +33,29 @@ serverPackages()
         npm i 
         echo "NPM packages installed"
         
-        mkdir "$PWD/data"
+        mkdir "$PWD/dist/data"
         echo "Created folder for database"
 
-        read -p "Would you like build the service? [Y/n]: " answer
-        answer="${answer:-Y}"
-        if [[ $answer =~ [Yy] ]]; then    
+        choice=$(getUserinput "Would you like to build the service?")
+        if [[ $choice =~ [Yy] ]]; then    
             npm run build
 
             setupService "/home/$user/.nvm/versions/node/v$NODE_VERSION/bin/node  $PWD/dist/app.js"
         fi
         
-        read -p "Would you like to allow port's $API_PORT and $WEB_PORT through ufw? [Y/n]: " answer
-        answer="${answer:-Y}"
-        if [[ $answer =~ [Yy] ]]; then
+        choice=$(getUserinput "Would you like to allow port's $API_PORT and $WEB_PORT through ufw?")
+        if [[ $choice =~ [Yy] ]]; then
             ufw allow "$API_PORT/tcp"
             ufw allow "$WEB_PORT/tcp"
         fi
     fi
 
-    chown -R $user:$user ../{PWD##*/} 
+    chown -R $user:$user "../${PWD##*/}"
 }
 
 setupService() {
-    read -p "Would you like to setup a systemd service? [Y/n]: " answer
-    answer="${answer:-Y}"
-    if [[ $answer =~ [Yy] ]]; then        
+    choice=$(getUserinput "Would you like to setup a systemd service?")
+    if [[ $choice =~ [Yy] ]]; then        
         service="[Unit]
             \nDescription=Watch up status to gracefully shutdown in case of prolonged power outage.
             \nAfter=network.target\n
@@ -78,15 +76,13 @@ setupService() {
             \n"
         echo -e $service > /etc/systemd/system/powersentinal.service
 
-        read -p "Would you like powersentinal.service to start on boot [Y/n]: " answer
-        answer="${answer:-Y}"
-        if [[ $answer =~ [Yy] ]]; then
+        choice=$(getUserinput "Would you like to start powersentinal.service to start on boot?")
+        if [[ $choice =~ [Yy] ]]; then
             systemctl enable powersentinal.service
         fi
 
-        read -p "Would you like to start powersentinal.service now? [Y/n]: " answer
-        answer="${answer:-Y}"
-        if [[ $answer =~ [Yy] ]]; then
+        choice=$(getUserinput "Would you like to start powersentinal.service now?")
+        if [[ $choice =~ [Yy] ]]; then
             systemctl daemon-reload
             systemctl start powersentinal.service
         fi
@@ -95,12 +91,14 @@ setupService() {
     fi
 }
 
-serverInstall() {
-    if [[ $@ =~ '-y' ]] ; then
-        
-    else 
-        serverPackages
-    fi
+getUserinput() {
+    if [[ $express_install == true ]] ; then
+        answer="y"
+    else
+        read -p "$1 [Y/n]: " answer
+        answer="${answer:-Y}"
+    fi 
+    echo $answer
 }
 
 startFromScratch() {
@@ -113,9 +111,24 @@ startFromScratch() {
     esac
 }
 
-case $1 in 
-    server|S|s) serverPackages ;;
-    client|C|c) setupService "$PWD/client_src/watcher.sh" ;;
-    *) startFromScratch
-esac
+while test $# != 0
+do
+    case $1 in
+        -y|-Y) express_install=true ;;
+        -s|-S) server_install=true ;;
+        -c|-C) client_install=true ;;
+    esac
+    shift
+done
+
+if [[ $server_install == true &&  $client_install == true ]] ; then
+    echo "Cannot use both -S (Server) and -C (Client) on the same system!"
+    exit 1
+elif [[ $server_install == false &&  $client_install == false ]] ; then
+    startFromScratch
+elif [[ $server_install == true &&  $client_install == false ]] ; then 
+    serverPackages
+elif [[ $server_install == false &&  $client_install == true ]] ; then 
+    setupService "$PWD/client_src/watcher.sh"
+fi
 
