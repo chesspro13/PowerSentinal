836387122dba7fa80c52aefa96a4e859dfa31d96
chesspro13 <chesspro13@gmail.com>
2024-09-29 17:48:48 -0700

    Updated watcher

--- watcher.sh
+++ watcher.sh
@@ -3,12 +3,12 @@ source .env
 
 while true 
 do
-    echo "Pinging server at http://$IP_ADDRESS:$API_PORT/status"
-    echo $(curl "http://$IP_ADDRESS:$API_PORT/status")
+    echo "Pinging server at http://$IP_ADDRESS:$API_PORT/api/status"
+    echo $(curl "http://$IP_ADDRESS:$API_PORT/api/status")
     
-    statusWquotes=$(curl "http://$IP_ADDRESS:$API_PORT/status" | jq .STATUS)
+    statusWquotes=$(curl "http://$IP_ADDRESS:$API_PORT/api/status" | jq .STATUS)
     status=${statusWquotes//\"/}
-    battLevel=$(curl "http://$IP_ADDRESS:$API_PORT/status" | jq .BCHARGE)
+    battLevel=$(curl "http://$IP_ADDRESS:$API_PORT/api/status" | jq .BCHARGE)
     battQuote=${battLevel// Percent/}
     percentage=${battQuote//\"/}
     level=${percentage%.*}
